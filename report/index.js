cbd7ea7d0d36d2636f40acd7dc98543cf7af981c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-14 22:52:52 -0400

    adding report virtual dom

---
+++ report/index.js
@@ -0,0 +1,34 @@
+const Cycle = require('@cycle/core')
+const Rx = require('rx')
+const {makeDOMDriver, pre, table, tr, td, span, div} = require('@cycle/dom')
+
+function coverageDom (coverage) {
+  return pre([
+    table('.coverage', [
+      tr([
+        td('.linecount .quiet', '1\n2\n3\n4\n5'),
+        td('.line-coverage .quiet', [
+          div('.cline-any .cline-yes', '1×'),
+          div('.cline-any .cline-neutral', ' '),
+          div('.cline-any .cline-yes', '1×'),
+          div('.cline-any .cline-yes', '2×'),
+          div('.cline-any .cline-neutral', ' ')
+        ])
+      ])
+    ])
+  ])
+}
+
+function view (coverage$) {
+  return coverage$.map(coverageDom)
+}
+
+function main ({DOM}) {
+  // no incoming events yet
+  const cover = {foo: 'bar'}
+  const coverage$ = Rx.Observable.just(cover)
+  return {
+    DOM: view(coverage$)
+  }
+}
+Cycle.run(main, { DOM: makeDOMDriver('#app') })
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = [{
+  output: {
+    path: './report',
+    filename: 'bundle.js'
+  },
+  entry: {
+    app: './report/index.js'
+  }
+}]
