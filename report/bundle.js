51eb04e924b4d6c6b7b3e6a706ab2109db355b1e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-14 23:15:16 -0400

    generating table of source lines

--- report/bundle.js
+++ report/bundle.js
@@ -48,28 +48,55 @@
 	const Rx = __webpack_require__(2)
 	const {makeDOMDriver, pre, table, tr, td, span, div} = __webpack_require__(5)
 
-	function coverageDom (coverage) {
-	  return pre([
-	    table('.coverage', [
-	      tr([
-	        td('.linecount .quiet', '1\n2\n3\n4\n5'),
-	        td('.line-coverage .quiet', [
-	          div('.cline-any .cline-yes', '1×'),
-	          div('.cline-any .cline-neutral', ' '),
-	          div('.cline-any .cline-yes', '1×'),
-	          div('.cline-any .cline-yes', '2×'),
-	          div('.cline-any .cline-neutral', ' ')
-	        ]),
-	        td('.text',
-	          pre('.lang-js', `// example program to be instrumented
+	const source = `// example program to be instrumented
 	function add(a, b) {
 	  return a + b
-	}`
-	            )
-	          )
-	      ])
-	    ])
+	}
+
+	function sub(a, b) {
+	  return a - b
+	}
+
+	function abs(x) {
+	  if (x < 0) {
+	    return -x
+	  }
+	  return x
+	}
+	console.log('2 + 3 =', add(2, 3))
+	`
+	const lines = source.split('\n')
+
+	function sourceLineToRow (sourceLine, index) {
+	  return tr('.line', [
+	    td('.linecount .quiet', String(index + 1)),
+	    td('.cline-any .cline-yes', '1×'),
+	    td('.text',
+	      pre('.lang-js', sourceLine)
+	    )
 	  ])
+	}
+
+	function coverageDom (coverage) {
+	  return table('.coverage', lines.map(sourceLineToRow))
+	  // return pre([
+	  //   table('.coverage', [
+	  //     tr([
+	  //       td('.linecount .quiet', '1\n2\n3\n4\n5'),
+	  //       td('.line-coverage .quiet', [
+	  //         div('.cline-any .cline-yes', '1×'),
+	  //         div('.cline-any .cline-neutral', ' '),
+	  //         div('.cline-any .cline-yes', '1×'),
+	  //         div('.cline-any .cline-yes', '2×'),
+	  //         div('.cline-any .cline-neutral', ' ')
+	  //       ]),
+	  //       td('.text',
+	  //         pre('.lang-js', source
+	  //           )
+	  //         )
+	  //     ])
+	  //   ])
+	  // ])
 	}
 
 	function view (coverage$) {
@@ -77,7 +104,7 @@
 	}
 
 	function main ({DOM}) {
-	  // no incoming events yet
+	  // no incoming events yet?
 	  const cover = {foo: 'bar'}
 	  const coverage$ = Rx.Observable.just(cover)
 	  return {
--- report/index.js
+++ report/index.js
@@ -2,28 +2,55 @@ const Cycle = require('@cycle/core')
 const Rx = require('rx')
 const {makeDOMDriver, pre, table, tr, td, span, div} = require('@cycle/dom')
 
-function coverageDom (coverage) {
-  return pre([
-    table('.coverage', [
-      tr([
-        td('.linecount .quiet', '1\n2\n3\n4\n5'),
-        td('.line-coverage .quiet', [
-          div('.cline-any .cline-yes', '1×'),
-          div('.cline-any .cline-neutral', ' '),
-          div('.cline-any .cline-yes', '1×'),
-          div('.cline-any .cline-yes', '2×'),
-          div('.cline-any .cline-neutral', ' ')
-        ]),
-        td('.text',
-          pre('.lang-js', `// example program to be instrumented
+const source = `// example program to be instrumented
 function add(a, b) {
   return a + b
-}`
-            )
-          )
-      ])
-    ])
+}
+
+function sub(a, b) {
+  return a - b
+}
+
+function abs(x) {
+  if (x < 0) {
+    return -x
+  }
+  return x
+}
+console.log('2 + 3 =', add(2, 3))
+`
+const lines = source.split('\n')
+
+function sourceLineToRow (sourceLine, index) {
+  return tr('.line', [
+    td('.linecount .quiet', String(index + 1)),
+    td('.cline-any .cline-yes', '1×'),
+    td('.text',
+      pre('.lang-js', sourceLine)
+    )
   ])
+}
+
+function coverageDom (coverage) {
+  return table('.coverage', lines.map(sourceLineToRow))
+  // return pre([
+  //   table('.coverage', [
+  //     tr([
+  //       td('.linecount .quiet', '1\n2\n3\n4\n5'),
+  //       td('.line-coverage .quiet', [
+  //         div('.cline-any .cline-yes', '1×'),
+  //         div('.cline-any .cline-neutral', ' '),
+  //         div('.cline-any .cline-yes', '1×'),
+  //         div('.cline-any .cline-yes', '2×'),
+  //         div('.cline-any .cline-neutral', ' ')
+  //       ]),
+  //       td('.text',
+  //         pre('.lang-js', source
+  //           )
+  //         )
+  //     ])
+  //   ])
+  // ])
 }
 
 function view (coverage$) {
@@ -31,7 +58,7 @@ function view (coverage$) {
 }
 
 function main ({DOM}) {
-  // no incoming events yet
+  // no incoming events yet?
   const cover = {foo: 'bar'}
   const coverage$ = Rx.Observable.just(cover)
   return {
