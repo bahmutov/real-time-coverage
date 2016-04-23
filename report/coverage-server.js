6084625baa9cb80d2e763d862449e9aa732d8ed2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-23 11:32:27 -0400

    feat(server): sending source and coverage to each connected client

--- report/coverage-server.js
+++ report/coverage-server.js
@@ -7,15 +7,28 @@ const read = require('fs').readFileSync
 const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({ port: 3032 })
 
+function sendCoverage (send) {
+  const filename = join(__dirname, './coverage.json')
+  const coverage = read(filename, 'utf8')
+  console.log('resetting code coverage', filename)
+  send({coverage})
+}
+
+function sendSource (send) {
+  const sourceName = join(__dirname, '../examples/calc.js')
+  const source = read(sourceName, 'utf8')
+  console.log('resetting source code', sourceName)
+  const filename = justName(sourceName) // for now
+  send({source, filename})
+}
+
 wss.on('connection', function connection (ws) {
-  console.log('connection!')
-
-  // TODO: send the full code coverage object right away
-
-  ws.on('message', function incoming (message) {
-    console.log('received: %s', message)
-  })
-  // ws.send('something')
+  console.log('new connection!')
+  function sendObject (o) {
+    ws.send(JSON.stringify(o))
+  }
+  sendSource(sendObject)
+  sendCoverage(sendObject)
 })
 wss.broadcast = function broadcast (data) {
   wss.clients.forEach(function each (client) {
@@ -28,28 +41,5 @@ setInterval(function () {
   console.log('sending random line', randomLine)
   wss.broadcast(JSON.stringify({line: randomLine}))
 }, 1000)
-
-function reset () {
-  return Math.random() < 1
-}
-
-setTimeout(function () {
-  if (reset()) {
-    const sourceName = join(__dirname, '../examples/calc.js')
-    const source = read(sourceName, 'utf8')
-    console.log('resetting source code', sourceName)
-    const filename = justName(sourceName) // for now
-    wss.broadcast(JSON.stringify({source, filename}))
-  }
-}, 5000)
-
-setTimeout(function () {
-  if (reset()) {
-    const filename = join(__dirname, './coverage.json')
-    const coverage = read(filename, 'utf8')
-    console.log('resetting code coverage', filename)
-    wss.broadcast(JSON.stringify({coverage}))
-  }
-}, 7000)
 
 console.log('running ws at port 3032')
